276ab1513a5d404354e7227fbfeaca631f4a8e51
Lee Aspitarte <aspitarte@gmail.com>
2021-09-27 11:37:29 -0700

    functionalize gen_idx_searchterm

--- s2orc/gen_index_abstract.py
+++ s2orc/gen_index_abstract.py
@@ -3,62 +3,43 @@
 import sqlite3
 import os
 import pandas as pd
+import nlp_utils as nu
 
 DATASET_DIR = r'C:\Users\aspit\Git\NLP-Semantic\datasets'
 db_path = os.path.join(DATASET_DIR, 'db_s2orc.db')
 con = sqlite3.connect(db_path)
 
 #TODO: I wonder if it might be faster doing first pass to see if there were any of the search terms, then search for each term in that subset.
-
-SEARCH_LIMIT = int(1e7)
-
-cursor = con.cursor()
-
 regexes = [
     'energy storage',
     'electricity storage',
     'lithium ion',
-    'lead acid',
-    'solid oxide fuel cell',
-    'compressed air',
-    'pumped thermal',
-    'thermomechanical',
-    'thermal energy storage', #just thermal would probably be a lot of articles...
-    'flywheel',
-    'superconducting magnetic',
-    'supercapacitor',
+    # 'lead acid',
+    # 'solid oxide fuel cell',
+    # 'compressed air',
+    # 'pumped thermal',
+    # 'thermomechanical',
+    # 'thermal energy storage', #just thermal would probably be a lot of articles...
+    # 'flywheel',
+    # 'superconducting magnetic',
+    # 'supercapacitor',
 ]
 
 regexes = ['%' + r + '%' for r in regexes]
 
 all_ids = []
-
 for regex in regexes:
 
     print('Searching for regex: ' + regex)
-
-    # put -- to comment out line
-    cursor.execute("""
-    SELECT paper_id FROM
-    --raw_text
-    (SELECT * FROM raw_text LIMIT {})
-    WHERE abstract LIKE '{}'
-    OR title like '{}'
-    """.format(SEARCH_LIMIT,regex,regex)
-        )
-    ## Just get count, seems to take as long. 
-    # cursor.execute("SELECT COUNT(*) FROM raw_text WHERE abstract LIKE '%flywheel energy storage%'")
-
-    results = cursor.fetchall()
-
-    print("Num Results: " + str(len(results)))
-
-    ids = [r[0] for r in results]
+    ids = nu.io.gen_ids_searchterm(con, regex, idx_name='paper_id', search_fields=['abstract', 'title'], search_limit=int(1e5))
     all_ids.append(ids)
+
 
 df = pd.DataFrame(all_ids).transpose()
 df.columns = regexes
 
-df.to_sql('indexed_searches', con, if_exists='replace')
+# df.to_sql('indexed_searches', con, if_exists='replace')
 
 con.close()
+
+df
