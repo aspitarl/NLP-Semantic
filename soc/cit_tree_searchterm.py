aa55e811ec8bea06443c6fe13cf652a2f377f8cd
Lee Aspitarte <aspitarte@gmail.com>
2021-09-30 16:52:45 -0700

    many changes to cit_tree
    revamp grpah generation
    add output plots

--- soc/cit_tree_searchterm.py
+++ soc/cit_tree_searchterm.py
@@ -1,5 +1,6 @@
 #%%
 
+from bokeh.models.renderers import GraphRenderer
 from bokeh.models.sources import ColumnDataSource
 import pandas as pd
 import xarray as xr
@@ -29,10 +30,20 @@ def set_round(G, round):
             G.nodes[node]['round'] = round
     return G
 
-regex = '%carbon nanotube%'
-ids = nu.io.gen_ids_searchterm(con, regex, idx_name='id', search_fields=['paperAbstract', 'title'], search_limit=int(5e5))
+regex = '%natural language processing%'
+ids = nu.io.gen_ids_searchterm(con, regex, idx_name='id', search_fields=['paperAbstract', 'title'], search_limit=int(5e6), output_limit=1000)
 
 #%%
+
+def trim_graph(G, n_max):
+    G = trim_graph_size(G, n_max*2)
+    G = trim_graph_num_edges(G, 1)
+    G = get_frac_connected(G, con)
+    G = trim_graph_fraction(G, n_max)
+    G = trim_graph_num_edges(G, 1)
+    return G
+
+all_Gs = []
 
 G = nx.Graph()
 G.add_nodes_from(ids, round=0)
@@ -40,18 +51,25 @@ G.add_nodes_from(ids, round=0)
 ## Initial internal citation trim
 G = gen_citation_tree(G, con, cit_field='inCitations', add_new=False)
 print("size of citation tree: {}".format(len(G.nodes)))
-G = trim_graph_size(G, 1000)
-G = trim_graph_num_edges(G, 1)
+
+G = nu.citation.trim_connected_components(G, 10)
+G = trim_graph(G, 100)
+
+all_Gs.append(G.copy())
 
 
+#%%
 #Grow citation graph including external citations
-for i, n_max in enumerate([1000,1000,1000,1000]):
+for i, n_max in enumerate([300,1000,3000,10000]):
     print('Round {}'.format(i+1))
     G = gen_citation_tree(G, con, cit_field='both', add_new=True)
     print("size of final citation tree: {}".format(len(G.nodes)))
-    G = trim_graph_size(G, n_max)
-    G = trim_graph_num_edges(G, 1)
+    G = trim_graph(G, n_max)
+
+    G = nu.citation.trim_connected_components(G, 1)
     set_round(G, i+1)
+
+    all_Gs.append(G.copy())
 
 
 print("Loading final database")
@@ -60,6 +78,28 @@ df = load_df_semantic(con, G.nodes)
 print("removing nodes not in database")
 G.remove_nodes_from([id for id in G.nodes if id not in df.index])
 
+nx.write_gexf(G, os.path.join('output','G_cit_tree.gexf'))
+#%%
+
+from fa2 import ForceAtlas2
+forceatlas2 = ForceAtlas2()
+#%%
+
+rounds_plot = [0,1,2,3,4]
+
+fig, axes = plt.subplots(1,len(rounds_plot), figsize=(15,5))
+
+for round in rounds_plot:
+    G_plot = all_Gs[round]
+    # pos = nx.spring_layout(G_plot)
+    pos = forceatlas2.forceatlas2_networkx_layout(G_plot, pos=None, iterations=10)
+    nx.draw_networkx_nodes(G_plot, pos, ax=axes[round], node_size=10)
+    nx.draw_networkx_edges(G_plot, pos, ax=axes[round],width=0.1)
+
+    axes[round].set_title('Round {}'.format(round))
+
+
+plt.savefig('output/cit_tree_rounds.png')
 
 #%%
 
@@ -67,14 +107,21 @@ from bokeh.plotting import figure, from_networkx
 from bokeh.io import output_notebook, show, output_file
 from bokeh.models import (BoxSelectTool, Circle, EdgesAndLinkedNodes, HoverTool,
                           MultiLine, NodesAndLinkedEdges, Plot, Range1d, TapTool,Legend,LegendItem, ColumnDataSource)
-from bokeh.palettes import Spectral4, Spectral5
+from bokeh.palettes import Spectral4, Spectral6
 
-cmap = Spectral5
+cmap = Spectral6
 
-plot = figure(plot_width=600, plot_height=600)
+rounds_plot = [0,1,2,3,4]
+nodes_plot = [node for node in G.nodes if G.nodes[node]['round'] in rounds_plot]
+G_plot = G.subgraph(nodes_plot)
+G_plot = nx.Graph(G_plot)
+# G_plot.remove_edges_from(G.edges) # Speed
+print(len(G_plot.nodes))
+
+plot = figure(plot_width=800, plot_height=600, output_backend='webgl')
 
 #Dummy markers for legend, doesn't appear to be a way to have a colored legend with networx plot...
-rounds = set(G.nodes[node]['round'] for node in G.nodes)
+rounds = set(G_plot.nodes[node]['round'] for node in G_plot.nodes)
 source = ColumnDataSource(dict(
     x = [0]*len(rounds),
     y = [0]*len(rounds),
@@ -85,25 +132,37 @@ source = ColumnDataSource(dict(
 plot.circle(source=source, legend_group = 'label', color = 'color', visible=False)
 
 # Networkx plot
-for node in G.nodes:
+for node in G_plot.nodes:
     # if node in df_2.index:
-    G.nodes[node]['title']= df.loc[node]['title']
-    G.nodes[node]['cit_round'] = cmap[G.nodes[node]['round']]
+    G_plot.nodes[node]['title']= df.loc[node]['title']
+    G_plot.nodes[node]['cit_round'] = cmap[G_plot.nodes[node]['round']]
 
-graph = from_networkx(G, nx.spring_layout)
 
-graph.node_renderer.glyph = Circle(size=15, fill_color='cit_round')
-# graph.node_renderer.selection_glyph = Circle(size=15, fill_color='gray')
-graph.node_renderer.hover_glyph = Circle(size=15, fill_color='gray')
-graph.edge_renderer.glyph = MultiLine(line_color='black', line_alpha=0.4, line_width=1)
+pos = forceatlas2.forceatlas2_networkx_layout(G_plot, pos=None, iterations=10)
+source = ColumnDataSource(dict(
+x = [pos[key][0] for key in pos.keys()],
+y = [pos[key][1] for key in pos.keys()],
+color = [cmap[G_plot.nodes[node]['round']] for node in pos.keys()],
+title = [G_plot.nodes[node]['title'] for node in pos.keys()],
+))
 
-plot.add_tools(HoverTool(tooltips=[('title', '@title')]), TapTool(), BoxSelectTool())
-plot.renderers.append(graph)
+plot.circle(source = source, color='color', radius = 3, radius_units='screen')
 
-output_notebook()
+plot.add_tools(HoverTool(tooltips=[('title', '@title')]))
+
+## from_networkx method doesn't seem to work with webgl
+# graph = from_networkx(G_plot, lambda G: forceatlas2.forceatlas2_networkx_layout(G, iterations=10))
+# graph.node_renderer.glyph = Circle(fill_color='cit_round')
+# # graph.node_renderer.hover_glyph = Circle(size=15, fill_color='gray')
+# # # graph.edge_renderer.glyph = MultiLine(line_color='black', line_alpha=0.4, line_width=1)
+# graph.edge_renderer.visible = False
+# plot.renderers.append(graph)
+
+# output_notebook()
+# show(plot)
+
+output_file(filename='output/cit_tree_bokeh.html', title = 'Initial Citation Tree Rounds', )
 show(plot)
-
-
 
 #%%
 print("Corex Topic Modeling")
@@ -156,7 +215,7 @@ year_range_plot = slice(1990,2020)
 
 nu.plot.top_slopes_plot(df_topicsyear.loc[year_range_plot], s_topic_words, year_range_fit, n_plots=30, highlight_topics=highlight_topics)
 
-
+plt.savefig('output/top_slopes_plot.png')
 
 
 
